988a4478e3a6196c64f15210ac116328875ff90a
alejoal07 <alejo_alvarez07@hotmail.com>
2023-11-21 19:45:34 -0400

    Add files via upload

--- udptunnel.c
+++ udptunnel.c
@@ -496,7 +496,7 @@ static int udp_to_tcp(struct relay *relay)
             ntohs(remote_udpaddr.sin_port));
     for(int i = 0; i<buflen ; i++){
       fprintf(stderr, "%02X ",p.buf[i]);
-      fprintf(stderr, "\n";   
+      fprintf(stderr, "\n");   
     }
   }
   p.length = htons(buflen);
